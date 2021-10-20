0714dcd0fcbd042d884c312a9923c5b874039213
Rosh_W <roshani.wright@hotmail.com>
2021-10-20 17:24:43 -0400

    saving

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/roshaniwright/Desktop/S4L/gatsby-starter-default/SDChallenge-1/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/roshaniwright/Desktop/S4L/gatsby-starter-default/SDChallenge-1/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/roshaniwright/Desktop/S4L/gatsby-starter-default/SDChallenge-1/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/roshaniwright/Desktop/S4L/gatsby-starter-default/SDChallenge-1/src/pages/page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/roshaniwright/Desktop/S4L/gatsby-starter-default/SDChallenge-1/src/pages/using-typescript.tsx"))
+}
+
